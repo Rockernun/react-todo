f1c9bbde99e5c01763032da6752401638c558878
Eokji coding <byunguk98@naver.com>
2024-01-24 22:05:00 +0900

    Input Form 생성
    To do(할 일) 입력 받고 입력창 reset하기

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,25 +1,29 @@
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface IForm {
+  toDo: string;
+}
 
 function ToDoList() {
-  const [todo, setToDo] = useState("");
-  
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {currentTarget: {value}
-  } = event;
-  setToDo(value);
-  };
-
-  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(todo);
-  };
-
+  const {
+    register, handleSubmit, setValue
+  } = useForm<IForm>();
+  const handleValid = (data: IForm) => {
+    console.log('add to do', data.toDo);
+    //  Form이 제출될 경우, input을 비운다. 첫 번째 인자로 input의 register에 넣어준 이름 사용, 2번 인자로 바꿔줄 input 값
+    setValue("toDo", ""); 
+  }
   return <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={todo} placeholder="Write a to do" />
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input {...register("toDo", {
+          required: "Please write a To Do",  //  에러에 메시지 표시
+        })} placeholder="Write a to do" />
         <button>Add</button>
       </form>
   </div>;
 }
+
+//  handleSubmit 함수를 사용할 때는 첫 번째 매개변수로 데이터가 유효할 때 호출되는 다른 함수를 받는 것.
+//  원한다면, 데이터가 유효하지 않을 때 호출될 다른 함수를 2번째 매개변수로 넣을 수도 있다.  
 
 export default ToDoList;
