d79964db3a816b48a2b63288dbb12f2940e3aca5
Eokji coding <byunguk98@naver.com>
2024-01-25 01:31:29 +0900

    To Do

---
+++ src/atoms.tsx
@@ -0,0 +1,36 @@
+import { atom, selector } from "recoil";
+
+export enum Categories {
+  "TO_DO" = "TO_DO",
+  "DOING" = "DOING",
+  "DONE" = "DONE",
+}
+
+export interface IToDo {  
+  text: string;
+  id: number;
+  category: Categories;
+}
+
+const localToDosJSON = localStorage.getItem("ToDos");
+const localToDos = JSON.parse(localToDosJSON as any);
+
+export const categoryState = atom<Categories>({
+  key: "category",
+  default: Categories.TO_DO,
+});
+
+
+export const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: localToDos,
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    return toDos ? toDos.filter((toDo) => toDo.category === category):[];
+  },
+});
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,48 +1,30 @@
-import { useForm } from "react-hook-form";
-import { atom, useRecoilState } from "recoil";
-import { StringLiteral } from "typescript";
-
-//  atom에 접근하려면, useRecoilValue 함수를 이용
-//  atom의 type이 ToDo의 배열임을 알려주기
-const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
-
-interface IForm {
-  toDo: string;
-}
-
-interface IToDo {  //  할 일, 하고 있는 일, 한 일
-  text: string;
-  id: number;
-  category: "TO_DO" | "DOING" | "DONE";
-}
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import {toDoSelector, categoryState} from "../atoms";
+import { Categories } from "../atoms";
 
 function ToDoList() {
-  const [toDos, setToDos] = useRecoilState(toDoState);  //  value랑 변경 함수 둘다 들고 있다.
-  const {
-    register, handleSubmit, setValue
-  } = useForm<IForm>();
-  const handleValid = ({toDo}: IForm) => {
-    setToDos(oldToDos => [{text:toDo, id:Date.now(), category:"TO_DO"}, ...oldToDos])  //  ...oldToDos는 배열 안의 요소를 반환
-    setValue("toDo", ""); 
-  };
-  return <div>
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value as any);
+  }
+  console.log(toDos);
+  return ( 
+  <div>
     <h1>To Dos</h1>
     <hr />
-      <form onSubmit={handleSubmit(handleValid)}>
-        <input {...register("toDo", {
-          required: "Please write a To Do", 
-        })} placeholder="Write a to do" />
-        <button>Add</button>
-      </form>
-      <ul>
-        {toDos.map(toDo => (
-        <li key={toDo.id}>{toDo.text}</li>
-        ))}
-      </ul>
-  </div>;
+    <select value={category} onInput={onInput}>
+        <option value={Categories.TO_DO}>To Do</option>
+        <option value={Categories.DOING}>Doing</option>
+        <option value={Categories.DONE}>Done</option>
+      </select>
+    <CreateToDo />
+    {toDos?.map(toDo => <ToDo key={toDo.id} {...toDo}/>)}
+  </div>
+  );
 }
 
 export default ToDoList;
